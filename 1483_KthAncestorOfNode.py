65139c2e865189b9bc727a2385357f9d44a0791a
Chenye Zhu <zhuchenye@outlook.com>
2020-07-28 03:49:11 -0700

    minor change

--- 1483_KthAncestorOfNode.py
+++ 1483_KthAncestorOfNode.py
@@ -11,7 +11,7 @@ from typing import List
 
 
 class TreeAncestor:
-    PRE_COMPUTE_MAX_STEP: int = 15
+    PRE_COMPUTE_MAX_JUMP: int = 50_000
     NONE_EXISTENCE: int = -1
 
     def __init__(self, n: int, parent: List[int]):
@@ -20,37 +20,47 @@ class TreeAncestor:
         :param n: n nodes numbered 0 to n-1
         :param parent: parent[i] is the parent of node i
         """
-        current_step_ancestor, current_step = dict(enumerate(parent)), 0
-        jump_map = [current_step_ancestor]
-        has_next_level = True
-        while current_step < self.PRE_COMPUTE_MAX_STEP and has_next_level:
-            has_next_level = False
-            next_step_ancestor = {}
-            for i in current_step_ancestor:
-                if current_step_ancestor.get(current_step_ancestor[i], self.NONE_EXISTENCE) != self.NONE_EXISTENCE:
-                    # Exists grandparent node
-                    next_step_ancestor[i] = current_step_ancestor[current_step_ancestor[i]]
-                    has_next_level = True
-            current_step_ancestor = next_step_ancestor
-            jump_map.append(current_step_ancestor)
-            current_step += 1
-        self.PRE_COMPUTE_MAX_STEP = current_step
+        # current_jump_ancestor is 2^current_jump-th ancestor
+        # initialize to current_jump = 0 and current_jump_ancestor is 1st ancestor
+        current_jump = 0
+        current_jump_ancestors = {i: parent_i for i, parent_i in enumerate(parent)}
+        jump_map = [current_jump_ancestors]
+        has_next_jump = True
+
+        while current_jump < self.PRE_COMPUTE_MAX_JUMP and has_next_jump:
+            has_next_jump = False
+            # next_jump_ancestor is 2^(current_jump+1)-th ancestor
+            next_jump_ancestor = {}
+            for i in current_jump_ancestors:
+                if current_jump_ancestors.get(current_jump_ancestors[i], self.NONE_EXISTENCE) != self.NONE_EXISTENCE:
+                    # Exists next jump node
+                    next_jump_ancestor[i] = current_jump_ancestors[current_jump_ancestors[i]]
+                    has_next_jump = True
+            current_jump_ancestors = next_jump_ancestor
+            jump_map.append(current_jump_ancestors)
+            current_jump += 1
+
+        self.PRE_COMPUTE_MAX_JUMP = current_jump
         self.jump_map = jump_map
 
     def getKthAncestor(self, node: int, k: int) -> int:
         """
         break down k into binaries of 1,0 and use jump_map for look up
             e.g. 5th (1001) ancestor = 1st ancestor of 4th ancestor of node
+        :param: node such that 0 <= node < n, 1 <= n <= 5*10^4
+        :param: 1 <= k <= 5*10^4
         :return: k-th ancestor of node if exists or else -1
         """
-        step = self.PRE_COMPUTE_MAX_STEP
+        jump = self.PRE_COMPUTE_MAX_JUMP
         while k > 0 and node >= 0:
             # find the leading 1 in binary representation of node
-            if k >= 1 << step:
-                node = self.jump_map[step].get(node, self.NONE_EXISTENCE)
-                k -= 1 << step
+            if k >= 1 << jump:
+                node = self.jump_map[jump].get(node, self.NONE_EXISTENCE)
+                if node == self.NONE_EXISTENCE:
+                    return self.NONE_EXISTENCE
+                k -= 1 << jump
             else:
-                step -= 1
+                jump -= 1
         return node
 
 
@@ -60,11 +70,11 @@ class TreeAncestor:
 test_obj = TreeAncestor(7, [-1, 0, 0, 1, 1, 2, 2])
 assert test_obj.getKthAncestor(3, 1) == 1
 assert test_obj.getKthAncestor(5, 2) == 0
-assert test_obj.getKthAncestor(6, 3) == -1
+assert test_obj.getKthAncestor(6, 3) == test_obj.NONE_EXISTENCE
 
 test_obj = TreeAncestor(5, [-1, 0, 0, 0, 3])
-assert test_obj.getKthAncestor(1, 5) == -1
-assert test_obj.getKthAncestor(3, 2) == -1
-assert test_obj.getKthAncestor(0, 1) == -1
+assert test_obj.getKthAncestor(1, 5) == test_obj.NONE_EXISTENCE
+assert test_obj.getKthAncestor(3, 2) == test_obj.NONE_EXISTENCE
+assert test_obj.getKthAncestor(0, 1) == test_obj.NONE_EXISTENCE
 assert test_obj.getKthAncestor(3, 1) == 0
-assert test_obj.getKthAncestor(3, 5) == -1
+assert test_obj.getKthAncestor(3, 5) == test_obj.NONE_EXISTENCE
