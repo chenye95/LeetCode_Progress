192284ce07a3533dcf022827733a79855061dfcc
Chenye Zhu <zhuchenye@outlook.com>
2021-03-01 00:59:47 -0800

    3/1 Progress
    575 Distribute Candies

---
+++ 575_DistributeCandies.py
@@ -0,0 +1,22 @@
+"""
+Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she
+visited a doctor.
+
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much,
+ and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if
+she only eats n / 2 of them.
+"""
+from typing import List
+
+
+def distribute_candies(candy_types: List[int]) -> int:
+    return min(len(candy_types) // 2, len(set(candy_types)))
+
+
+test_cases = [([1, 1, 2, 2, 3, 3], 3),
+              ([1, 1, 2, 3], 2),
+              ([6, 6, 6, 6], 1), ]
+for test_input, expected_output in test_cases:
+    assert distribute_candies(test_input) == expected_output
