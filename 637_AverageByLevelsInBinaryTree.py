224065faf8d6278e30854893716b21871333f45b
Chenye Zhu <zhuchenye@outlook.com>
2021-03-06 01:48:44 -0800

    3/5 Progress
    637 Average By Levels In Binary Tree

---
+++ 637_AverageByLevelsInBinaryTree.py
@@ -0,0 +1,38 @@
+"""
+Given a non-empty binary tree, return the average value of the nodes on each level in the form of an array.
+"""
+from typing import List
+
+from _Binary_Tree import ConstructTree, TreeNode
+
+
+def average_of_levels(root: TreeNode) -> List[float]:
+    """
+    BFS solution
+    :param root: root of binary tree
+    :return: list of float, each represent average of node values in a level
+    """
+    if not root:
+        return []
+
+    current_level = [root]
+    list_avg = []
+
+    while current_level:
+        current_level_sum = 0.0
+        next_level = []
+        for node in current_level:
+            current_level_sum += node.val
+            if node.left:
+                next_level.append(node.left)
+            if node.right:
+                next_level.append(node.right)
+
+        list_avg.append(current_level_sum / len(current_level))
+        current_level = next_level
+
+    return list_avg
+
+
+test_tree = ConstructTree.build_tree_leetcode([3, 9, 20, None, None, 15, 7])
+assert average_of_levels(test_tree.root) == [3, 14.5, 11]
