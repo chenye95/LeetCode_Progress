13e1abb76a13b20518e530ff8a145f4a690b717e
Chenye Zhu <zhuchenye@outlook.com>
2021-04-25 03:56:56 -0700

    4/25 Progress
    48 Rotate Image

--- 11_ContainerWithMaxWater.py
+++ 11_ContainerWithMaxWater.py
@@ -7,6 +7,10 @@ from typing import List
 
 
 def max_area(height: List[int]) -> int:
+    """
+    :param height: n non-negative integers, each represents a vertical line at point i with height[i]
+    :return: most water contained by any two vertical lines which together with x-axis form a container
+    """
     max_area_tracker = 0
     left_pointer, right_pointer = 0, len(height) - 1
     while left_pointer < right_pointer:
@@ -20,7 +24,12 @@ def max_area(height: List[int]) -> int:
     return max_area_tracker
 
 
-test_cases = [([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+test_cases = [([76, 155, 15, 188, 180, 154, 84, 34, 187, 142, 22, 5, 27, 183, 111, 128, 50, 58, 2, 112, 179, 2, 100,
+                111, 115, 76, 134, 120, 118, 103, 31, 146, 58, 198, 134, 38, 104, 170, 25, 92, 112, 199, 49, 140, 135,
+                160, 20, 185, 171, 23, 98, 150, 177, 198, 61, 92, 26, 147, 164, 144, 51, 196, 42, 109, 194, 177, 100,
+                99, 99, 125, 143, 12, 76, 192, 152, 11, 152, 124, 197, 123, 147, 95, 73, 124, 45, 86, 168, 24, 34, 133,
+                120, 85, 81, 163, 146, 75, 92, 198, 126, 191], 18048),
+              ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
               ([1, 1], 1),
               ([4, 3, 2, 1, 4], 16),
               ([1, 2, 1], 2), ]
---
+++ 48_RotateImage.py
@@ -0,0 +1,53 @@
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate
+ another 2D matrix and do the rotation.
+"""
+from copy import deepcopy
+from typing import List
+
+
+def rotate_set_of_four(matrix: List[List[int]]) -> None:
+    """
+    :param matrix: None empty square matrix, n x n, to be rotated clockwise, in place
+    """
+    n = len(matrix)
+    # divide the matrix into 4 sections
+    for i in range((n + 1) // 2):
+        for j in range(n // 2):
+            tmp = matrix[n - 1 - j][i]  # bottom_left corner
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]  # bottom_left takes bottom_right
+            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]  # bottom_right takes top_right
+            matrix[j][n - 1 - i] = matrix[i][j]  # top_right takes top_left
+            matrix[i][j] = tmp  # top_left takes bottom_left
+
+
+def rotate_transpose_reflect(matrix: List[List[int]]) -> None:
+    """
+    :param matrix: None empty square matrix, n x n, to be rotated clockwise, in place
+    """
+    n = len(matrix)
+
+    # Transpose around main diagonal
+    for i in range(n):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Flip around the vertical mid point
+    for i in range(n):
+        matrix[i].reverse()
+
+
+test_cases = [([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]],
+               [[21, 16, 11, 6, 1], [22, 17, 12, 7, 2], [23, 18, 13, 8, 3], [24, 19, 14, 9, 4], [25, 20, 15, 10, 5]]),
+              ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[7, 4, 1], [8, 5, 2], [9, 6, 3]]),
+              ([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]],
+               [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]),
+              ([[1]], [[1]]),
+              ([[1, 2], [3, 4]], [[3, 1], [4, 2]]), ]
+for rotate in [rotate_transpose_reflect, rotate_set_of_four, ]:
+    for test_matrix, expected_output in test_cases:
+        copy_matrix = deepcopy(test_matrix)
+        rotate(copy_matrix)
+        assert copy_matrix == expected_output, rotate.__name__
