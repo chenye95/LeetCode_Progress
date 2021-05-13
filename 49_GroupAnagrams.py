42ad4f080660af11d704e8092c373fd007a49957
Chenye Zhu <zhuchenye@outlook.com>
2021-05-12 23:38:30 -0700

    5/13 Progress - YiQi
    283 Move Zeroes
    49 Group Anagrams
    5 Longest Palindromic Substring
    239 Sliding Window Maximum

--- 49_GroupAnagrams.py
+++ 49_GroupAnagrams.py
@@ -19,15 +19,26 @@ def group_anagrams(str_list: List[str]) -> List[List[str]]:
     return [anagram_map[key] for key in anagram_map]
 
 
-for test_input, expected_output in [(["eat", "tea", "tan", "ate", "nat", "bat"],
-                                     [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]),
-                                    ([""], [[""]]),
-                                    (["a"], [["a"]]), ]:
-    value_out = group_anagrams(str_list=test_input)
-    expected_map = {}
-    for one_list in expected_output:
-        expected_map[tuple(sorted(one_list[0]))] = set(one_list)
-    value_map = {}
-    for one_list in value_out:
-        value_map[tuple(sorted(one_list[0]))] = set(one_list)
+test_cases = [(["eat", "tea", "tan", "ate", "nat", "bat"],
+               [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]),
+              ([""], [[""]]),
+              (["a"], [["a"]]),
+              (["ray", "cod", "abe", "ned", "arc", "jar", "owl", "pop", "paw", "sky", "yup", "fed", "jul", "woo", "ado",
+                "why", "ben", "mys", "den", "dem", "fat", "you", "eon", "sui", "oct", "asp", "ago", "lea", "sow", "hus",
+                "fee", "yup", "eve", "red", "flo", "ids", "tic", "pup", "hag", "ito", "zoo"],
+               [["zoo"], ["hag"], ["pup"], ["ids"], ["tic"], ["flo"], ["red"], ["fee"], ["hus"], ["fed"], ["sky"],
+                ["yup", "yup"], ["ben"], ["pop"], ["you"], ["abe"], ["den", "ned"], ["cod"], ["jul"], ["jar"], ["ray"],
+                ["paw"], ["owl"], ["oct"], ["woo"], ["ado"], ["dem"], ["eve"], ["why"], ["sui"], ["ito"], ["ago"],
+                ["arc"], ["mys"], ["fat"], ["eon"], ["asp"], ["lea"], ["sow"]]),
+              (["eat", "tea", "tan", "ate", "nat", "bat", "ac", "bd", "aac", "bbd", "aacc", "bbdd", "acc", "bdd"],
+               [["bdd"], ["bat"], ["nat", "tan"], ["ac"], ["ate", "eat", "tea"], ["bd"], ["aac"], ["bbd"], ["aacc"],
+                ["bbdd"], ["acc"]]),
+              (["abets", "bead", "remain", "betas", "abed", "baste", "airline", "leading", "beast", "dealing", "beats",
+                "airmen", "marine", "single", "bade", "aligned"],
+               [["single"], ["abets", "baste", "beast", "beats", "betas"], ["abed", "bade", "bead"],
+                ["airmen", "marine", "remain"], ["airline"], ["aligned", "dealing", "leading"]]),
+              ]
+for test_input, expected_output in test_cases:
+    expected_map = {tuple(sorted(one_list[0])): set(one_list) for one_list in expected_output}
+    value_map = {tuple(sorted(one_list[0])): set(one_list) for one_list in group_anagrams(str_list=test_input)}
     assert value_map == expected_map
--- 5_LongestPalindromicString.py
+++ 5_LongestPalindromicString.py
@@ -1,6 +1,7 @@
 """
 Given a string s, return the longest palindromic substring in s.
 """
+from typing import Tuple
 
 
 def longest_palindrome_dp(s: str) -> str:
@@ -26,7 +27,7 @@ def longest_palindrome_dp(s: str) -> str:
     if not return_str:
         return_str = s[0]
 
-    for p_len in range(3, len(s)):
+    for p_len in range(3, len(s) + 1):
         for i in range(0, len(s) - p_len + 1):
             j = i + p_len - 1
             memory[i][j] = memory[i + 1][j - 1] and (s[i] == s[j])
@@ -42,32 +43,39 @@ def longest_palindromic_construction(s: str) -> str:
 
     :return: longest palindromic substring in s
     """
-    def expand_around_center(l: int, r: int) -> int:
-        while 0 <= l and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return r - l - 1  # (r - 1) - (l + 1) + 1
+
+    def expand_around_center(left_s: int, right_s: int) -> Tuple[int, int, int]:
+        """
+        :return: length the longest palindromic substring expanded around s[left_s: right_s + 1], and start and end of
+            the substring
+        """
+        while 0 <= left_s and right_s < len(s) and s[left_s] == s[right_s]:
+            left_s -= 1
+            right_s += 1
+        # (right_s - 1) - (left_s + 1) + 1
+        return right_s - left_s - 1, left_s + 1, right_s - 1
 
     if not s or len(s) <= 1:
         return s
 
     return_str_start, return_str_end, return_str_len = 0, 0, 1
     for i in range(len(s)):
-        len_1 = expand_around_center(i, i)  # mirror around position i
-        len_2 = expand_around_center(i, i + 1)  # mirror around position i, i + 1
-        i_len = max(len_1, len_2)
+        option_odd_len = expand_around_center(i, i)  # mirror around position i
+        option_even_len = expand_around_center(i, i + 1)  # mirror around position i, i + 1
+        i_len, new_left, new_right = max(option_odd_len, option_even_len)
         if i_len > return_str_len:
-            return_str_len = i_len
-            return_str_start = i - (i_len - 1) // 2
-            return_str_end = i + i_len // 2
+            return_str_len, return_str_start, return_str_end = i_len, new_left, new_right
 
     return s[return_str_start: return_str_end + 1]
 
 
-test_cases = [("babad", {"bab", "aba"}),
+test_cases = [("ccc", {"ccc"}),
+              ("babad", {"bab", "aba"}),
               ("cbbd", {"bb"}),
               ("a", {"a"}),
-              ("ac", {"a", "c"}), ]
-for longest_palindromic in [longest_palindromic_construction, longest_palindrome_dp]:
+              ("ac", {"a", "c"}),
+              ("aacabdkacaa", {"aca", }),
+              ("xaabacxcabaaxcabaax", {"xaabacxcabaax", })]
+for longest_palindromic in [longest_palindromic_construction, longest_palindrome_dp, ]:
     for test_input, expected_output in test_cases:
-        assert longest_palindromic(s=test_input) in expected_output
+        assert longest_palindromic(s=test_input) in expected_output, longest_palindromic.__name__
