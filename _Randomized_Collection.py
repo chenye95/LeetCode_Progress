eb30c570f4e80c588355e833dceaca9968eaba78
Chenye Zhu <zhuchenye@outlook.com>
2021-05-27 14:40:05 -0700

    Update Randomized Implementation
    Randomized expect Hashable objects

--- _Randomized_Collection.py
+++ _Randomized_Collection.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import Any
+from typing import Hashable, Optional, Dict, Set
 
 
 class RandomizedCollection:
@@ -7,11 +7,11 @@ class RandomizedCollection:
         """
         Supports weighted randomly chooses one elements from the collection
         """
-        self.values_in_collections = {}
-        self.position_to_value_lookup = dict()
-        self.total_counter = 0
+        self.values_in_collections: Dict[Hashable, Set[int]] = {}
+        self.position_to_value_lookup: Dict[int, Hashable] = dict()
+        self.total_counter: int = 0
 
-    def insert(self, val: Any) -> bool:
+    def insert(self, val: Hashable) -> bool:
         """
         Inserts a value to the collection or increment its weight by 1
 
@@ -26,7 +26,7 @@ class RandomizedCollection:
         self.total_counter += 1
         return flag_new_obj
 
-    def remove(self, val: Any) -> bool:
+    def remove(self, val: Hashable) -> bool:
         """
         Decrease weight of val by 1 if it exists
 
@@ -35,22 +35,30 @@ class RandomizedCollection:
         """
         if val not in self.values_in_collections:
             return False
+
         val_idx = self.values_in_collections[val].pop()
-        if not self.values_in_collections[val]:  # No more val in the collections
+        if not self.values_in_collections[val]:
+            # No more val in the collections
             del self.values_in_collections[val]
 
-        last_val = self.position_to_value_lookup[self.total_counter - 1]
         if val_idx != self.total_counter - 1:
+            last_val = self.position_to_value_lookup[self.total_counter - 1]
             self.values_in_collections[last_val].remove(self.total_counter - 1)
             self.values_in_collections[last_val].add(val_idx)
             self.position_to_value_lookup[val_idx] = last_val
         else:
             del self.position_to_value_lookup[self.total_counter - 1]
+
         self.total_counter -= 1
         return True
 
-    def get_random(self) -> Any:
+    def get_random(self) -> Optional[Hashable]:
         """
         Get a random element from the collection according to its weigh
+
+        :return: an object if the collection is not empty or None otherwise
         """
-        return self.position_to_value_lookup[randint(0, self.total_counter - 1)]
+        if self.total_counter:
+            return self.position_to_value_lookup[randint(0, self.total_counter - 1)]
+        else:
+            return None
