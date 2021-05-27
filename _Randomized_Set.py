d7551ee58d515e47644e58f6a9f44f6155bf67b8
Chenye Zhu <zhuchenye@outlook.com>
2021-05-27 14:40:05 -0700

    Update Randomized Implementation
    Randomized expect Hashable objects

--- _Randomized_Set.py
+++ _Randomized_Set.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import Any
+from typing import Hashable, Optional, List, Dict
 
 
 class RandomizedSet:
@@ -7,41 +7,47 @@ class RandomizedSet:
         """
         Supports unweighted randomly chooses one elements from the collection
         """
-        self.obj_in_collections = []
-        self.pos_to_obj_lookup = {}
+        self.obj_in_collections: List[Hashable] = []
+        self.obj_to_pos_lookup: Dict[Hashable, int] = {}
 
-    def insert(self, val: Any) -> bool:
+    def insert(self, val: Hashable) -> bool:
         """
         :param val: inserts a value to the set
         :return: if the set did not already contain the specified element.
         """
-        if val in self.pos_to_obj_lookup:
+        if val in self.obj_to_pos_lookup:
             return False
 
+        self.obj_to_pos_lookup[val] = len(self.obj_in_collections)
         self.obj_in_collections.append(val)
-        self.pos_to_obj_lookup[val] = len(self.obj_in_collections) - 1
         return True
 
-    def remove(self, val: Any) -> bool:
+    def remove(self, val: Hashable) -> bool:
         """
         :param val: removes a value from the set
-        :return if the set contained the specified element.
+        :return: if the set contained the specified element.
         """
-        if val not in self.pos_to_obj_lookup:
+        if val not in self.obj_to_pos_lookup:
             return False
 
-        idx, lst = self.pos_to_obj_lookup[val], self.obj_in_collections[-1]
-        self.obj_in_collections[idx], self.pos_to_obj_lookup[lst] = lst, idx
+        val_index = self.obj_to_pos_lookup[val]
+        if val_index != len(self.obj_in_collections) - 1:
+            # move last object in the list to fill the gap of val
+            last_object = self.obj_in_collections[-1]
+            self.obj_in_collections[val_index] = last_object
+            self.obj_to_pos_lookup[last_object] = val_index
+
         self.obj_in_collections.pop()
-        del self.pos_to_obj_lookup[val]
+        del self.obj_to_pos_lookup[val]
         return True
 
-    def get_random(self) -> Any:
+    def get_random(self) -> Optional[Hashable]:
         """
         Get a unweighted random element from the set
-        :return an object if the collection is not empty or None otherwise
+
+        :return: an object if the collection is not empty or None otherwise
         """
-        if not self.obj_in_collections:
-            return None
-        else:
+        if self.obj_in_collections:
             return self.obj_in_collections[randint(0, len(self.obj_in_collections) - 1)]
+        else:
+            return None
--- _Randomized_test.py
+++ _Randomized_test.py
@@ -5,21 +5,44 @@ from _Randomized_Set import RandomizedSet
 
 # Your RandomizedCollection object will be instantiated and called as such:
 obj = RandomizedCollection()
-assert obj.insert(1)
-assert not obj.insert(1)
-assert obj.insert(2)
+assert obj.get_random() is None
+assert obj.insert(1) is True
+assert obj.insert(1) is False
+assert obj.insert(2) is True
 rand_counter = Counter()
 for _ in range(1000):
     rand_counter[obj.get_random()] += 1
-print(rand_counter)
+assert rand_counter[1] + rand_counter[2] == 1000
+assert 666 - 40 <= rand_counter[1] <= 666 + 40
+assert 334 - 40 <= rand_counter[2] <= 334 + 40
+assert obj.remove(1) is True
+assert obj.get_random() in {1, 2}
+assert obj.remove(1) is True
+assert obj.get_random() == 2
+assert obj.remove(1) is False
+assert obj.get_random() == 2
+assert obj.insert(1) is True
+assert obj.remove(2) is True
+assert obj.get_random() == 1
 # param_3 = obj_in_collections.get_random()
 
 # Your RandomizedSet object will be instantiated and called as such:
 obj = RandomizedSet()
-assert obj.insert(1)
-assert obj.insert(2)
+assert obj.get_random() is None
+assert obj.insert(1) is True
+assert obj.get_random() == 1
+assert obj.insert(1) is False
+assert obj.get_random() == 1
+assert obj.insert(2) is True
 assert obj.get_random() in {1, 2}
-assert not obj.insert(2)
-assert obj.remove(1)
-assert not obj.remove(1)
+random_counter_2 = Counter()
+for _ in range(1000):
+    random_counter_2[obj.get_random()] += 1
+assert random_counter_2[1] + random_counter_2[2] == 1000
+assert 500 - 50 <= random_counter_2[1] <= 500 + 50
+assert 500 - 50 <= random_counter_2[2] <= 500 + 50
+assert obj.insert(2) is False
+assert obj.get_random() in {1, 2}
+assert obj.remove(1) is True
+assert obj.remove(1) is False
 assert obj.get_random() == 2
