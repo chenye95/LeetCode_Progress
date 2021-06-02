9a9d53fafa0c5ca1c499e84e5797a04b8cf281b8
Chenye Zhu <zhuchenye@outlook.com>
2021-06-01 19:25:10 -0700

    Add search words start with prefix to Trie structure

--- _Trie.py
+++ _Trie.py
@@ -70,3 +70,30 @@ class Trie:
                 return False
 
         return True
+
+    def get_words_start_with(self, prefix: str, max_recommendation: int):
+        """
+        :param prefix: string starts with, case sensitive, prefix. Does not support self.chr_match_all_letters
+        :param max_recommendation: max length of the returned list
+        :return: list of words, in alphabetical order, that starts with prefix
+        """
+
+        def dfs_explore_words(exploring_node: defaultdict, word_so_far: str) -> None:
+            for c in sorted(exploring_node.keys()):
+                if c != self.end_of_word:
+                    dfs_explore_words(exploring_node[c], word_so_far + c)
+                else:
+                    words_list.append(word_so_far)
+                if len(words_list) >= max_recommendation:
+                    return
+
+        current_node = self.root
+        for c in prefix:
+            if c not in current_node:
+                return []
+            else:
+                current_node = current_node[c]
+
+        words_list = []
+        dfs_explore_words(current_node, prefix)
+        return words_list
