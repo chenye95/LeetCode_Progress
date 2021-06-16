59fd04f3e3449296c17a12205836cab98478da06
Chenye Zhu <zhuchenye@outlook.com>
2021-06-16 07:41:27 -0700

    6/14 Progress - YiQi
    392. Is Subsequence
    376. Wiggle Subsequence
    659. Split Array into Consecutive Subsequences
    321. Create Maximum Number

---
+++ 392_IsSubsequence.py
@@ -0,0 +1,42 @@
+"""
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the
+ characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of
+ "abcde" while "aec" is not).
+"""
+
+
+def is_subsequence(s: str, t: str) -> bool:
+    """
+    :param s: 1 <= len(s) <= 100, consists of lower case english letters only
+    :param t: 1 <= len(s) <= 10000, consists of lower case english letters only
+    :return: whether s is a subsequence of t
+    """
+    if not s:
+        return True
+    i = 0
+    for t_j in t:
+        if t_j == s[i]:
+            i += 1
+            if i == len(s):
+                return True
+    return False
+
+
+test_cases = [("abc", "ahbgdc", True),
+              ("axc", "ahbgdc", False),
+              ("bcd",
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu" +
+               "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubcdadcd",
+               True), ]
+for test_s, test_t, expected_value in test_cases:
+    assert is_subsequence(test_s, test_t) is expected_value
